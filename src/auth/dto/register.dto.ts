dcf9c83424ffd37526c05fab7e987e3d9b7b602e
nyoosteven <stevenchrist123@gmail.com>
2023-12-04 12:28:02 +0700

    feat: add email validation

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
 
 export class RegisterDTO {
   @IsString()
   @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
 
   @IsString()
